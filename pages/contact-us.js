9488f83ca41a8e231a3b33e77070b6f775639df1
Harsh <harshnag23@gmail.com>
2023-03-22 21:23:07 +0530

    Contact us page almost done

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useState } from "react";
 
 function ContactUs() {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <div>
       <section className="m-5">
@@ -12,7 +17,11 @@ function ContactUs() {
             Got a technical issue? Want to send feedback about a beta feature?
             Need details about our Business plan? Let us know.
           </p>
-          <form action="#" className="space-y-8">
+          <form
+            action="https://formsubmit.co/jigsawgames23@gmail.com"
+            method="POST"
+            className="space-y-8"
+          >
             <div>
               <label
                 for="email"
@@ -22,7 +31,12 @@ function ContactUs() {
               </label>
               <input
                 type="email"
+                name="email"
                 id="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="name@flowbite.com"
                 required
@@ -37,7 +51,12 @@ function ContactUs() {
               </label>
               <input
                 type="text"
+                name="subject"
                 id="subject"
+                value={subject}
+                onChange={(e) => {
+                  setSubject(e.target.value);
+                }}
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Let us know how we can help you"
                 required
@@ -52,12 +71,20 @@ function ContactUs() {
               </label>
               <textarea
                 id="message"
+                name="message"
                 rows="6"
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
               ></textarea>
             </div>
-            <button className="px-8 py-4 w-full text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
+            <button
+              className="px-8 py-4 w-full text-lg font-medium text-center text-white bg-indigo-600 rounded-md "
+              type="submit"
+            >
               Submit Query
             </button>
           </form>
