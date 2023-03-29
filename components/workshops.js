01007b5bc5ce432241985e543276de2631c068aa
Harsh <harshnag23@gmail.com>
2023-03-29 11:30:15 +0530

    Workshop section edited

--- components/workshops.js
+++ components/workshops.js
@@ -12,36 +12,37 @@ function Workshops() {
         help connect to the source within and to tap into the innate abilities
         to shape the life as the person wants
       </SectionTitle>
-
-      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-10 justify-center self-center">
-        <a href="#">
-          <img
-            class="rounded-t-lg"
-            src="https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness.jpeg"
-            alt=""
-          />
-        </a>
-        <div class="p-5">
+      <div className="flex flex-row justify-evenly">
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-10 justify-center self-center">
           <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Bhaav Retreat{" "}
-            </h5>
+            <img
+              class="rounded-t-lg"
+              src="https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness.jpeg"
+              alt=""
+            />
           </a>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Bhaav - Dimensions of life “Happiness is your choice no fate”
-            .This Retreat is for the alignment of mind body and soul. The true
-            nature of souls is happiness “satchitananda”. Once we connect with
-            that true nature the happiness slowly flows to other dimension of
-            our being called mind. Once the mind is filled the happiness slowly
-            reflects through the body and actions.{" "}
-          </p>
-          <a
-        //   add link of workshop of th gform here
-            href="#"
-            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Register Now !
-          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Bhaav Retreat{" "}
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Bhaav - Dimensions of life “Happiness is your choice no fate”
+              .This Retreat is for the alignment of mind body and soul. The true
+              nature of souls is happiness “satchitananda”. Once we connect with
+              that true nature the happiness slowly flows to other dimension of
+              our being called mind. Once the mind is filled the happiness
+              slowly reflects through the body and actions.{" "}
+            </p>
+            <a
+              //   add link of workshop of th gform here
+              href="#"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Register Now !
+            </a>
+          </div>
         </div>
       </div>
     </div>
