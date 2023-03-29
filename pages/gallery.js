75f03b77d996eb19e8a3c1fbf76a97a38be21328
Harsh <harshnag23@gmail.com>
2023-03-30 00:04:04 +0530

    Fade in and Fade out feature added on to the gallery page

--- pages/gallery.js
+++ pages/gallery.js
@@ -3,126 +3,150 @@ import Image from "next/image";
 import Img1 from "../public/img/cosmic_healing.jpg";
 import SectionTitle from "../components/sectionTitle";
 import Video from "../components/video";
+import FadeInSection from "../components/fadeInSection";
 
 function Gallery() {
   return (
     <div className="">
-      <SectionTitle
-        pretitle="Images"
-        title="Images of Cosmic Light Healer"
-      ></SectionTitle>
+      <FadeInSection>
+        <SectionTitle
+          pretitle="Images"
+          title="Images of Cosmic Light Healer"
+        ></SectionTitle>
+      </FadeInSection>
+
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4 m-5">
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
-        <div>
-          <Image
-            className="h-auto max-w-full rounded-lg"
-            src={Img1}
-            alt=""
-            layout="responsive"
-          />
-        </div>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Image
+              className="h-auto max-w-full rounded-lg"
+              src={Img1}
+              alt=""
+              layout="responsive"
+            />
+          </div>
+        </FadeInSection>
       </div>
       <SectionTitle
         pretitle="Videos"
         title="Videos of Cosmic Light Healer"
       ></SectionTitle>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4 m-5">
-        <div>
-          <Video />
-        </div>
-        <div>
-          <Video />
-        </div>
-        <div>
-          <Video />
-        </div>
+        <FadeInSection>
+          <div>
+            <Video />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Video />
+          </div>
+        </FadeInSection>
+        <FadeInSection>
+          <div>
+            <Video />
+          </div>
+        </FadeInSection>
       </div>
     </div>
   );
