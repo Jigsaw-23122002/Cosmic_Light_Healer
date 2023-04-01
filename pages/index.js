57c4e24b0c67435936a8f96032d91defef84a67f
Smit <smitsekhadia99@gmail.com>
2023-04-01 12:57:01 +0530

    removed video from home page

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,6 @@ export default function Home() {
       <Carousels />
       <FadeInSection>
         <SectionTitle
-          pretitle="Overview"
-          title="Essence of Yoga"
-        ></SectionTitle>
-        <p className="py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300 text-justify xl:ml-16 xl:mr-16 lg:ml-10 lg:mr-10 ml-5 mr-5">
-          Welcome to Cosmic Light Healer, your online destination for all things
-          yoga. Whether you're a seasoned yogi or just starting out, our website
-          offers a wealth of resources and information to help you deepen your
-          practice and achieve your goals.
-        </p>
-      </FadeInSection>
-      <FadeInSection>
-        <Video />
-      </FadeInSection>
-      <FadeInSection>
-        <SectionTitle
           pretitle="Testimonials"
           title="Experiences of Individuals with Cosmic Light healer"
         ></SectionTitle>
